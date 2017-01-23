c58c85c2399eb7b5e9a355e8622fb3ba73576541
alexander <alexander.efremov@apical.co.uk>
2017-01-23 11:00:26 +0000

    bugfix: in case if configured consumer couldn't be found and offsets are undefined, we will return array [0] instead of failing with error. todo: better to show message based on error status from burrow

--- client/utils/offsets-normalizer.js
+++ client/utils/offsets-normalizer.js
@@ -1,3 +1,7 @@
-export default function(offsets) {
-  return offsets.map((value) => value >= 0 ? value : 0)
+export default function (offsets) {
+    if (offsets) {
+        return offsets.map((value) => value >= 0 ? value : 0)
+    } else {
+        return [0]
+    }
 }
